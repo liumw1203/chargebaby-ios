bf1f5dde1982a64c9fd419facd6bdeee87d83b57
liumw1203 <liumw1203@aliyun.com>
2016-11-10 14:54:13 +0800

    Initial Commit

---
+++ chargebaby/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chargebaby
+//
+//  Created by 刘明伟 on 2016/11/10.
+//  Copyright © 2016年 刘明伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
